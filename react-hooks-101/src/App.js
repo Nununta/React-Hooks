ceca1cfddb4f8032dd4f3fe93275927f8daa61a4
Nununta <yutayuki09040827@gmail.com>
2021-04-04 16:37:17 +0900

    name,price初期化

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
 
 
-const App = () => {
-  const initialStates = {
-    name: '',
-    price: 1000,
+const App = props => {
+
+  const[name, setName] = useState(props.name);
+  const[price, setPrice] = useState(props.price);
+
+  const reset = () =>{
+    setPrice(props.price);
+    setName(props.name);
   }
-  const[name, setName] = useState(initialStates.name);
 
   return (
     <React.Fragment>
-  
+      <p>現在の{name}は{price}円です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input  value={name} onChange={e => setName(e.target.value)}/>
     </React.Fragment>
   );
+}
+
+
+App.defaultProps = {
+  name: 'yuta',
+  price: 1000
 }
 
 export default App;
