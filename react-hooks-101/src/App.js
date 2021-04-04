b1d7045560e14c94eba000437910321fcf2cf7a2
Nununta <yutayuki09040827@gmail.com>
2021-04-04 17:18:09 +0900

    リファクタリング

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = props => {
+    const[state, setState] = useState(props); 
+
+    const reset = () => {
+      setState(props);
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <p>現在の{state.name}は{state.price}円です。</p>
+      <button onClick={() => setState({...state, price:state.price + 1})}>+1</button>
+      <button onClick={() => setState({...state, price:state.price - 1})}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input  value={state.name} onChange={e => setState({...state, name: e.target.value})}/>
+    </React.Fragment>
   );
+}
+App.defaultProps = {
+  name:'ラーメン',
+  price:1000,
 }
 
 export default App;
