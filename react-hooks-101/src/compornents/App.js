704bd79f85ff17d3da80982fc3eb8474dd9fabfd
Nununta <yutayuki09040827@gmail.com>
2021-04-05 08:24:39 +0900

    bootstrapインストール　form実装

--- react-hooks-101/src/compornents/App.js
+++ react-hooks-101/src/compornents/App.js
@@ -1,46 +1,49 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
-const App = props => {
-    const[state, setState] = useState(props); 
-    const reset = () => {
-      setState(props);
-    }
+const App = () => {
 
-    useEffect(()=>{
-      console.log('useEffect');
-      //Domがレンダリングするたびに呼ばれる。
-    })
-
-    useEffect(()=>{
-      console.log('useEffect firstonly');
-      //Domがレンダリングされた一回のみ呼ばれる。
-    },[])
-
-    useEffect(()=>{
-      console.log('useEffect nameChangeonly');
-      //nameがレンダリングされた一回のみ呼ばれる。
-    },[state.name])
-
-    // const renderPeriod = () => {
-    //   console.log('render period');
-    //   return '。';
-    // }
 
   return (
-    <React.Fragment>
-      <p>現在の{state.name}は{state.price}円です。</p>
-      <button onClick={() => setState({...state, price:state.price + 1})}>+1</button>
-      <button onClick={() => setState({...state, price:state.price - 1})}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <input  value={state.name} onChange={e => setState({...state, name: e.target.value})}/>
-    </React.Fragment>
+   <div className="container-fluid">
+     <h4>イベント作成フォーム</h4>
+    <form>
+      <div className="form-group">
+        <label htmlFor="formEventTitle">タイトル</label>
+        <input className="form-control" id="formEventTitle" />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="formEventBody">ボディー</label>
+        <textarea className="form-control" id="formEventBody" />
+      </div>
+      <button className="btn btn-primary">イベントを作成する</button>
+      <button className="btn btn-danger">全てのイベントを削除する</button>
+    </form>
+    
+    <h4>イベント一覧</h4>
+    <table className="table table-hover">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>タイトル</th>
+          <th>ボディー</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+
+      </tbody>
+
+    </table>
+
+
+
+   </div>
   );
 }
-App.defaultProps = {
-  name:'ラーメン',
-  price:1000,
-}
+
 
 export default App;
