31ad967de1359062c9f4e0c548f845bac1a5944d
Nununta <yutayuki09040827@gmail.com>
2021-04-04 15:45:45 +0900

    commit

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -2,16 +2,37 @@ import React, { useState } from 'react';
 
 
 const App = () => {
-  const initialstates = {
-    name: "",
-    price: 1000
-  }
-
-  const [name, setName] = useState(initialstates.name); 
+  const [count, setCount] = useState(0);
+  // console.log({count});
+  // console.log({setCount});
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const double = () => setCount(count * 2);
+  const increment2 = () => setCount(previousCount => previousCount + 1);
+  const decrement2 = () => setCount(previousCount => previousCount - 1);
+  const divide3 = () => setCount(count => {
+    if(count % 3 === 0){
+      return count / 3;
+    }else {
+      return count;
+    }
+  });
+  const Reset = () => setCount(0);
 
   return (
     <React.Fragment>
-      
+    <div>Count:{count}</div>
+    <button onClick={increment}>+1</button>
+    <button onClick={decrement}>-1</button>
+    <div>
+      <button onClick={increment2}>+1</button>
+      <button onClick={decrement2}>-1</button>
+    </div>
+    <div>
+      <button onClick={Reset}>Reset</button>
+      <button onClick={double}>×2</button>
+      <button onClick={divide3}>3の倍数の時だけ割る</button>
+    </div>
     </React.Fragment>
   );
 }
